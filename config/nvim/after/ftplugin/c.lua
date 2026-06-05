3d632fe7b5fb65055b41530180e197ba5e5e384e
Drivingh0me <drivingh0me@icloud.com>
2026-06-04 19:40:36 -0500

    fixed

---
+++ config/nvim/after/ftplugin/c.lua
@@ -0,0 +1,13 @@
+-- Set number of space characters per tab
+vim.opt.tabstop = 4
+
+-- Set spaces per indentation level
+vim.opt.shiftwidth = 4
+
+vim.opt.softtabstop = 4
+
+vim.opt.expandtab = true
+
+vim.opt.autoindent = true
+
+vim.opt.smartindent = true
